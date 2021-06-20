9485d56c8271f07206efd2bc0f0a796c4c379fff
Ritu <djangopycon@gmail.com>
2021-06-20 23:59:31 +0530

    created youtubevideo and abstract model to store the youtube videos details

--- youtube/models.py
+++ youtube/models.py
@@ -1,25 +1,26 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 
 class YouTubeBaseModel(models.Model):
-	created_on = models.DateTimeField(auto_now_add=True)
-	updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
-	class Meta:
-		abstract = True
+    class Meta:
+        abstract = True
+
 
 class YouTubeVideo(YouTubeBaseModel):
     video_id = models.CharField(max_length=100, primary_key=True, unique=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
-    published_at = models.DateTimeField(null=True, blank=True)
-    thumbnails = models.JSONField(null=True)
+    published_at = models.DateTimeField(null=True)
+    thumbnails = JSONField(null=True)
+
+    class Meta:
+        db_table = "youtube_video"
+        verbose_name = "Youtube Video"
+        verbose_name_plural = "Youtube Videos"
 
     def __str__(self):
-    	return self.title
-
-	class Meta:
-		ordering = ["-published_at"]
-		db_table = "youtube_video"
-		verbose_name = "Youtube Video"
-		verbose_name_plural = "Youtube Videos"
+        return self.title
