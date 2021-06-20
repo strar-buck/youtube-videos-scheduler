d0c7238db30f46f5c7b18e9b76b324db89aeee5c
Ritu <djangopycon@gmail.com>
2021-06-20 21:01:08 +0530

    added entrypoint script for database migration and running collectstatic for static files

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
+python3 manage.py test --pattern="tests_*.py"
+
+exec "$@"
