4ea40dfd4cdea4448624f5af8109d47c9e04b9c4
Ritu <djangopycon@gmail.com>
2021-06-21 14:34:27 +0530

    maintaining constants in a separate file

---
+++ youtube/constants.py
@@ -0,0 +1,14 @@
+YOUTUBE_SEARCH_BASE_API_URL = "https://www.googleapis.com/youtube/v3/search/"
+DEFAULT_SEARCH_QUERY = "cricket"
+MAX_QUERY_RESULTS = 1000
+
+# STATUS CODE
+HTTP_200_OK = 200
+HTTP_403_FORBIDDEN = 403
+HTTP_401_UNAUTHORIZED = 401
+
+ACTIVE_API_KEY_NOT_FOUND = "No Active api key found"
+DEACTIVATED_API_KEY = "The Api key has been deactivated"
+EXHAUSTED_API_KEY = "The Api key has been exhausted"
+
+VIDEOS_SAVED_SUCCESSFULLY = "Videos has been saved successfully into db"
