ede69b2d126f7cdde084dfee32dee7fd91011fad
Ritu <djangopycon@gmail.com>
2021-06-21 19:26:44 +0530

    added prod and staging settings

---
+++ fampay/settings/prod.py
@@ -0,0 +1,28 @@
+from decouple import config
+import os
+
+from .base import *
+
+DEBUG = False
+# put the allowed hosts here
+ALLOWED_HOSTS = []
+
+# Database Settings
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("DB_NAME"),
+        "USER": config("DB_USER"),
+        "PASSWORD": config("DB_PASS"),
+        "HOST": config("DB_HOST"),
+        "PORT": config("DB_PORT"),
+    }
+}
+# DJANGO SECRET KEY
+SECRET_KEY = config("SECRET_KEY")
+
+###### Celery Configuration ###############
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_BROKER_URL = "redis://"
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_BACKEND = None
