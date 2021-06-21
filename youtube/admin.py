4299472d79353ecbc6884d7f1263b179d80b1e41
Ritu <djangopycon@gmail.com>
2021-06-21 14:38:14 +0530

    register models on the admin panel

--- youtube/admin.py
+++ youtube/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import YoutubeVideoCredential, YoutubeVideo
+
+admin.site.register(YoutubeVideoCredential)
+admin.site.register(YoutubeVideo)
